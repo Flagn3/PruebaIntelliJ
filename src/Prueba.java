f39cf274fe4f1ee08593e2219a4474402b508cd2
Flagn3 <flagn3@gmail.com>
2025-02-03 14:07:05 +0100

    Mensaje otra vez

--- src/Prueba.java
+++ src/Prueba.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Prueba {
@@ -5,6 +7,7 @@ public class Prueba {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!\n");
+        Scanner sc=new Scanner(System.in);
 
         int suma=0;
         for (int i = 1; i <= 5; i++) {
